1177023f79e1743d0e30510f0a89061c4842b717
Lukas <lukas_m@gmx.de>
2018-12-31 18:04:40 +0100

    Performance optimizations.
    Decreased update time for distance measurement by 25%.

--- BB-1_Velocity_Distance.ino
+++ BB-1_Velocity_Distance.ino
@@ -22,14 +22,19 @@
 #define HCSR04_TRIGGER_PIN 6	// pin connected to HC-SR04 trigger
 
 #define TRIGGER_TIME 20					// time in microseconds trigger is set to high
-#define TRIGGER_TIME_INTERVAL	2000	// maximum trigger time interval in TRIGGER_TIME
+#define TRIGGER_TIME_INTERVAL	1500	// maximum trigger time interval in TRIGGER_TIME
 
-//volatile boolean M1_interrupt = false;
-//volatile boolean M2_interrupt = false;
-//volatile boolean timer = false;
+//volatile boolean enc_M1_interrupt = false;
+//volatile boolean enc_M2_interrupt = false;
 
 volatile int16_t enc_count_M1 = 0;	// motor 1 encoder count
 volatile int16_t enc_count_M2 = 0;	// motor 2 encoder count
+
+volatile boolean front_meas = false;	// stores if a new measurement is available for front HC-SR04
+volatile boolean rear_meas = false;		// stores if a new measurement is available for rear HC-SR04
+
+volatile uint16_t front_echo_duration;	// front echo duration in microseconds
+volatile uint16_t rear_echo_duration;	// rear echo duration in microseconds
 
 volatile uint8_t front_distance = 255;	// distance to obstacle front in centimeter
 volatile uint8_t rear_distance = 255;	// distance to obstacle rear in centimeter
@@ -53,7 +58,7 @@ const int8_t lookup_table[] = {0, 0, 0, 0, 0, -1, 1, 0, 0, 1, -1, 0, 0, 0, 0, 0}
 
 void requestEvent() {
 	static byte message[6];
-		
+	
 	message[0] = (enc_count_M1 >> 8) & 0xFF;	// most significant bits for motor 1 encoder count
 	message[1] = enc_count_M1 & 0xFF;			// least significant bits for motor 1 encoder count
 	message[2] = (enc_count_M2 >> 8) & 0xFF;	// most significant bits for motor 2 encoder count
@@ -68,7 +73,7 @@ void requestEvent() {
 	enc_count_M2 = 0;
 }
 
-void encoder_isr_M1() {
+void enc_isr_M1() {
 	// stores the current and previous state of both encoder channels (A, B)
 	static uint8_t enc_state = 0;
 	
@@ -80,10 +85,10 @@ void encoder_isr_M1() {
 	// update encoder count according to the encoder state
 	enc_count_M1 = enc_count_M1 + lookup_table[enc_state & 0b1111];
 	
-	//M1_interrupt = true;
+	//enc_M1_interrupt = true;
 }
 
-void encoder_isr_M2() {
+void enc_isr_M2() {
 	// stores the current and previous state of both encoder channels (A, B)
 	static uint8_t enc_state = 0;
 	
@@ -95,45 +100,42 @@ void encoder_isr_M2() {
 	// update encoder count according to the encoder state
 	enc_count_M2 = enc_count_M2 - lookup_table[enc_state & 0b1111];
 	
-	//M2_interrupt = true;
-}
-
-// rear HC-SR04
-ISR(PCINT0_vect) {
-	static uint32_t echo_start;	// rear echo start in microseconds
-	static uint32_t echo_end;	// rear echo end in microseconds
-	static uint16_t echo_duration;	// rear echo duration in microseconds
-	
-	switch (digitalRead(8)) {
-		case HIGH:	// echo pin change was a rising edge (start of echo pulse)
-			echo_start = micros();
-			break;
-		case LOW:	// echo pin change was a falling edge (end of echo pulse)
-			echo_end = micros();
-			echo_duration = constrain(echo_end - echo_start, 0, 14790);	// calculate echo pulse duration and constrain it to measure a maximum distance of 255 cm (255 * 58 = 14790)
-			rear_distance = echo_duration / 58;	// calculate rear distance
-			break;
-	}
+	//enc_M2_interrupt = true;
 }
 
 // front HC-SR04
 ISR(PCINT2_vect) {
 	static uint32_t echo_start;	// echo start in microseconds
-	static uint32_t echo_end;	// echo end in microseconds
-	static uint16_t echo_duration;	// echo duration in microseconds
 	
 	switch (digitalRead(7)) {
 		case HIGH:	// echo pin change was a rising edge (start of echo pulse)
 			echo_start = micros();
 			break;
 		case LOW:	// echo pin change was a falling edge (end of echo pulse)
-			echo_end = micros();
-			echo_duration = constrain(echo_end - echo_start, 0, 14790);	// calculate echo pulse duration and constrain it to measure a maximum distance of 255 cm (255 * 58 = 14790)
-			front_distance = echo_duration / 58;	// calculate front distance
+			// calculate front echo pulse duration and constrain it to measure a maximum distance of 255 cm (255 * 58 = 14790)
+			front_echo_duration = constrain(micros() - echo_start, 0, 14790);
+			front_meas = true;
 			break;
 	}
 }
 
+// rear HC-SR04
+ISR(PCINT0_vect) {
+	static uint32_t echo_start;	// rear echo start in microseconds
+	
+	switch (digitalRead(8)) {
+		case HIGH:	// echo pin change was a rising edge (start of echo pulse)
+			echo_start = micros();
+			break;
+		case LOW:	// echo pin change was a falling edge (end of echo pulse)
+			// calculate rear echo pulse duration and constrain it to measure a maximum distance of 255 cm (255 * 58 = 14790)
+			rear_echo_duration = constrain(micros() - echo_start, 0, 14790);
+			rear_meas = true;
+			break;
+	}
+}
+
+// generate trigger used for front and rear HC-SR04
 void timer_isr() {
 	static uint16_t trigger_time_count = 0;	// counter for TRIGGER_TIME_INTERVAL
 	static uint8_t state = 1;	// state variable
@@ -153,7 +155,6 @@ void timer_isr() {
 		case 2:		// finish trigger pulse
 			digitalWrite(HCSR04_TRIGGER_PIN, LOW);	// set trigger pin to low
 			state = 0;
-			//timer = true;
 			break;
 	}
 }
@@ -174,8 +175,8 @@ void setup() {
 	pinMode(2, INPUT);	// interrupt pin for encoder M1
 	pinMode(3, INPUT);	// interrupt pin for encoder M2
 		
-	attachInterrupt(digitalPinToInterrupt(2), encoder_isr_M1, CHANGE);	// attach interrupt service routine encoder_isr_M1 to encoder M1 interrupt pin
-	attachInterrupt(digitalPinToInterrupt(3), encoder_isr_M2, CHANGE);	// attach interrupt service routine encoder_isr_M2 to encoder M2 interrupt pin
+	attachInterrupt(digitalPinToInterrupt(2), enc_isr_M1, CHANGE);	// attach interrupt service routine enc_isr_M1 to encoder M1 interrupt pin
+	attachInterrupt(digitalPinToInterrupt(3), enc_isr_M2, CHANGE);	// attach interrupt service routine enc_isr_M2 to encoder M2 interrupt pin
 	
 	// prepare HC-SR04 pins
 	pinMode(HCSR04_TRIGGER_PIN, OUTPUT);	// configure trigger pin as output
@@ -198,20 +199,29 @@ void setup() {
 }
 
 void loop() {
-	/*if (M1_interrupt) {
+	if (front_meas) {
+		front_meas = false;
+		// calculate front distance, which is limited to 255, since front_echo_duration is limited to 58 * 255 = 14790
+		front_distance = front_echo_duration / 58;
+		
+		//DEBUG_PRINTLN(front_distance);
+	}
+	
+	if (rear_meas) {
+		rear_meas = false;
+		// calculate rear distance, which is limited to 255, since rear_echo_duration is limited to 58 * 255 = 14790
+		rear_distance = rear_echo_duration / 58;
+		
+		//DEBUG_PRINT("\t"); DEBUG_PRINTLN(rear_distance);
+	}
+	
+	/*if (enc_M1_interrupt) {
 		DEBUG_PRINTLN(enc_count_M1 % 1000);
-		M1_interrupt = false;
+		enc_M1_interrupt = false;
 	}
 	
-	if (M2_interrupt) {
+	if (enc_M2_interrupt) {
 		DEBUG_PRINT("\t"); DEBUG_PRINTLN(enc_count_M2 % 1000);
-		M2_interrupt = false;
-	}
-	
-	if (timer == true) {
-		timer = false;
-
-		DEBUG_PRINT("\t"); DEBUG_PRINT("\t"); DEBUG_PRINT(front_distance);
-		DEBUG_PRINT("\t"); DEBUG_PRINTLN(rear_distance);
+		enc_M2_interrupt = false;
 	}*/
 }
